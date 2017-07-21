685c23a02255eff44f69e560a55c6a6415a63feb
bgando <gandolfo.bianca@gmail.com>
2017-07-21 11:41:40 -0500

    Update template

--- index.js
+++ index.js
@@ -2,89 +2,106 @@ const xlsx = require('node-xlsx');
 const fs = require('fs');
 const Handlebars = require('handlebars');
 
-const obj = xlsx.parse(__dirname + '/test-w-import.xlsx');
-const data = obj[0].data;
-data.splice(0,1);
+const obj = xlsx.parse(__dirname + '/can-migrate-transforms-clean-v8.xlsx');
+const rows = obj[0].data; //all rows
+rows.splice(0,1); //remove headings
 
 Handlebars.registerHelper('toLowerCase', function(str) {
   return str.toLowerCase();
 });
 
 
-// const source = '{{#each data}}{{#each this}}<div>{{this}}</div>{{/each}}{{/each}}';
-
 const source =
-'{{#each data as |component index|}}\n'+
-'### {{component.[0]}}\n'+
+' \\\\THIS IS A GENERATED FILE. SEE ____\n' +
+'{{#each rows as |component|}}\n'+
+'### {{{component.[0]}}}\n'+
 ' \n'+
-'To run [all of the {{component.[0]}} transforms](https://github.com/canjs/can-migrate/tree/master/src/transforms/{{component.[0]}}) listed below:\n'+
+'To run [all of the {{{component.[0]}}} transforms](https://github.com/canjs/can-migrate/tree/master/src/transforms/{{{component.[0]}}}) listed below:\n'+
 ' \n'+
 '```\n'+
-'can-migrate -a **/*.js -t {{component.[0]}}/\n'+
+'can-migrate -a **/*.js -t {{{component.[0]}}}/\n'+
 '```\n'+
 ' \n'+
-'#### {{component.[0]}}/replace\n'+
+'{{#if component.[11]}}\n'+ //if there is a replace.js script
+'#### {{{component.[0]}}}/replace\n'+
 ' \n'+
 'Running this transform:\n'+
 ' \n'+
 '```\n'+
-'can-migrate -a **/*.js -t {{component.[0]}}/replace.js\n'+
+'can-migrate -a **/*.js -t {{{component.[0]}}}/replace.js\n'+
 '```\n'+
 ' \n'+
 '…will transform code like this:\n'+
 ' \n'+
 '```js\n'+
 'import can from "can";\n'+
-'{{component.[2]}}(42);\n'+
+'{{#if component.[4]}}\n'+ //if there is a code snippet, use it
+'{{{component.[4]}}}\n'+
+'{{/if}}\n'+
 '```\n'+
 ' \n'+
 '…to this:\n'+
 ' \n'+
 '```js\n'+
-'import {{component.[1]}} from "{{component.[0]}}";\n'+
+'import {{{component.[3]}}} from "{{{component.[2]}}}";\n'+
+'{{#if component.[5]}}\n'+ //if there is a code snippet, use it
+'{{{component.[5]}}}\n'+
+'{{/if}}\n'+
 '```\n'+
-'{{#if component.[3]}}\n'+
-'#### {{component.[0]}}/import\n' +
+'{{/if}}\n'+
+'{{#if component.[6]}}\n'+ //if there are import/require transforms
+'#### {{{component.[0]}}}/import\n' +
 'Running this transform:\n' +
 '```\n' +
-'can-migrate -a **/*.js -t {{component.[0]}}/import.js\n' +
+'can-migrate -a **/*.js -t {{{component.[0]}}}/import.js\n' +
 '```\n' +
 '...will transform any of the following:\n' +
 '```js\n' +
-'import {{component.[1]}} from "can/{{toLowerCase component.[1]}}/";\n' +
-'import {{component.[1]}} from "can/{{toLowerCase component.[1]}}/{{toLowerCase component.[1]}}";\n' +
-'import {{component.[1]}} from "can/{{toLowerCase component.[1]}}/{{toLowerCase component.[1]}}.js";\n' +
+'{{#unless component.[7]}}\n' + //if the URL is normal, default
+'import {{{component.[1]}}} from "can/{{{toLowerCase component.[1]}}}/";\n' +
+'import {{{component.[1]}}} from "can/{{{toLowerCase component.[1]}}}/{{{toLowerCase component.[1]}}}";\n' +
+'import {{{component.[1]}}} from "can/{{{toLowerCase component.[1]}}}/{{{toLowerCase component.[1]}}}.js";\n' +
+'{{else}}\n' + //otherwise get paths
+'{{log "else"}}' +
+'{{log component.[8]}}'+
+'import {{{component.[1]}}} from "can/{{{toLowerCase component.[8]}}}/{{{toLowerCase component.[9]}}}/";\n' +
+'import {{{component.[1]}}} from "can/{{{toLowerCase component.[8]}}}/{{{toLowerCase component.[9]}}}/{{{toLowerCase component.[9]}}}";\n' +
+'import {{{component.[1]}}} from "can/{{{toLowerCase component.[8]}}}/{{{toLowerCase component.[9]}}}/{{{toLowerCase component.[9]}}}.js";\n' +
+'{{/unless}}\n' +
 '```\n' +
 '...to this:\n' +
 '```js\n' +
-'import {{component.[1]}} from "{{component.[0]}}";\n' +
+'import {{{component.[3]}}} from "{{{component.[2]}}}";\n' +
 '```\n' +
-'#### {{component.[0]}}/require\n' +
+'#### {{{component.[0]}}}/require\n' +
 'Running this transform:\n' +
 '```\n' +
-'can-migrate -a **/*.js -t {{component.[0]}}/require.js\n' +
+'can-migrate -a **/*.js -t {{{component.[0]}}}/require.js\n' +
 '```\n' +
 '...will transform any of the following:\n' +
 '```js\n' +
-'const {{component.[1]}} = require("can/{{toLowerCase component.[1]}}/");\n' +
-'const {{component.[1]}} = require(can/{{toLowerCase component.[1]}}/{{toLowerCase component.[1]}}");\n' +
-'const {{component.[1]}} = require( "can/{{toLowerCase component.[1]}}/{{toLowerCase component.[1]}}.js");\n' +
+'{{#unless component.[7]}}\n' + //if the URL is normal, default
+'const {{{component.[1]}}} = require("can/{{{toLowerCase component.[1]}}}/");\n' +
+'const {{{component.[1]}}} = require("can/{{{toLowerCase component.[1]}}}/{{{toLowerCase component.[1]}}}");\n' +
+'const {{{component.[1]}}} = require("can/{{{toLowerCase component.[1]}}}/{{{toLowerCase component.[1]}}}.js");\n' +
+'{{else }}\n' + //otherwise get paths
+'{{log "else"}}' +
+'{{log component.[8]}}' +
+'const {{{component.[1]}}} = require("can/{{{toLowerCase component.[8]}}}/{{{toLowerCase component.[9]}}}/");\n' +
+'const {{{component.[1]}}} = require("can/{{{toLowerCase component.[8]}}}/{{{toLowerCase component.[9]}}}/{{{toLowerCase component.[9]}}}");\n' +
+'const {{{component.[1]}}} = require("can/{{{toLowerCase component.[8]}}}/{{{toLowerCase component.[9]}}}/{{{toLowerCase component.[9]}}}.js");\n' +
+'{{/unless }}\n' +
 '```\n' +
 '...to this:\n' +
 '```js\n' +
-'const {{component.[1]}} = require("{{component.[0]}}");\n' +
+'const {{{component.[3]}}} = require("{{{component.[2]}}}");\n' +
 '```\n' +
 '{{/if}}\n'+
 '{{/each}}';
 
 const template = Handlebars.compile(source);
-
-console.log(data)
-
-for(let i = 0; i < data; i++) {
-
-} 
-const contents = template({data: data});
+ 
+const contents = template({rows: rows});
 
 fs.writeFile('contents.html', contents, err => {
     if (err) {
